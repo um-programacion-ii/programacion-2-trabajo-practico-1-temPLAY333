056f3e60be9e5e02eb5af3eec7e464235955b4f6
temPLAY333 <t.bourguet@alumno.um.edu.ar>
2025-04-09 11:43:43 -0300

    trabajo terminado

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,22 +4,30 @@ import main.modelo.Vehiculo;
 import main.servicio.VehiculoPrinter;
 import main.servicio.VehiculoService;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Crea un vehiculo, un camion y un Auto, he imprime sus datos
+        // Crea una lista de vehículos y los imprime
         VehiculoService vehiculoService = VehiculoService.getInstance();
         VehiculoPrinter vehiculoPrinter = VehiculoPrinter.getInstance();
 
-        Vehiculo vehiculo = vehiculoService.crearVehiculo("ABC123", "Toyota", 2020, 1500.0);
+        Vehiculo vehiculo1 = vehiculoService.crearVehiculo("ABC123", "Toyota", 2020, 1500.0);
         Vehiculo camion = vehiculoService.crearCamion("DEF456", "Ford", 2018, 5000.0, true);
         Vehiculo auto = vehiculoService.crearAuto("GHI789", "Honda", 2021, 1200.0, 5);
 
-        // Imprime los datos de los vehiculos
-        System.out.println("Datos del vehiculo:");
-        vehiculoPrinter.imprimirInformacion(vehiculo);
-        System.out.println("\nDatos del camion:");
-        vehiculoPrinter.imprimirInformacion(camion);
-        System.out.println("\nDatos del auto:");
-        vehiculoPrinter.imprimirInformacion(auto);
+        vehiculoService.agregarVehiculo(vehiculo1);
+        vehiculoService.agregarVehiculo(camion);
+        vehiculoService.agregarVehiculo(auto);
+
+        // Imprime los datos de los vehículos
+        for (Vehiculo vehiculo : vehiculoService.getVehiculos()) {
+            vehiculoPrinter.imprimirInformacion(vehiculo);
+            System.out.println();
+        }
+
+        // Imprime la información de un vehículo específico por patente
+        String patenteBuscada = "JKL012";
+        vehiculoPrinter.infoVehiculoPorPatente(vehiculoService.getVehiculos(), patenteBuscada);
     }
 }
